f6cfc649ec4faaf1d3a322d78c4f32d4b44d174f
DariussemR <igornite3d@gmail.com>
2026-03-10 21:24:11 -0300

    Sistema de interações - Inicial
    Sistema básico de interação, ainda não funcionando, apenas mudança de cenário e Timelines

---
+++ Scenes/item_behavior.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+#var interact_cursor = preload()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+#func _mouse_enter() -> void:
+	#Input.set_custom_mouse_cursor()
