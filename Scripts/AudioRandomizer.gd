a55f7312bce7dca2fd55e36fdb66e271fb9a1b75
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2026-03-27 15:03:39 -0300

    cena com chuva e buzina

---
+++ Scripts/AudioRandomizer.gd
@@ -0,0 +1,24 @@
+## Dá play no audio num tempo randomizado dentro de um intervalo específico.
+## Precisa de um AudioStreamRandomizer
+class_name AudioRandomizer
+extends AudioStreamPlayer
+
+@export var first_time_wait: float = 5
+@export var minimum_interval: float = 1
+@export var maximum_interval: float = 10
+
+@onready var timer: Timer = Timer.new()
+
+func _ready() -> void:
+	add_child(timer)
+	timer.start(first_time_wait)
+	await timer.timeout
+	start_timer()
+	timer.timeout.connect(timeout)
+
+func timeout() -> void:
+	play()
+	start_timer()
+	
+func start_timer() -> void:
+	timer.start(randf_range(minimum_interval, maximum_interval))
