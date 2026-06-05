05df3ae05dca2916759c3b057802c62f8fae892c
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2026-06-05 01:14:20 -0300

    interação com os itens ajustada
    agora o sinal de clicar e soltar o botão tá conectado com as funções no script

--- Scripts/Mechanics/Objects/item_behavior.gd
+++ Scripts/Mechanics/Objects/item_behavior.gd
@@ -2,7 +2,7 @@ extends TextureButton
 class_name Item
 ## Script para configurarmos os itens que forem criados
 #@onready var col_area:= $collision_area
-@onready var shader_outline: ShaderMaterial = self.material # será removido
+#@onready var shader_outline: ShaderMaterial = self.material # será removido
 ## Pra colocar um tipo para o item, default sendo 'leek'
 @export_category("Parâmetros de Interação")
 @export var item_type: String
@@ -10,7 +10,7 @@ class_name Item
 @export var delete_after_interaction:= true
 @export var open_after_interaction:= false
 @export var notebook: Notebook
-@export var hovered_texture: Texture2D # será removido
+#@export var hovered_texture: Texture2D # será removido
 
 var object_held:= false
 
@@ -21,8 +21,9 @@ func _ready() -> void:
 	#col_area.connect("mouse_exited", _reset_cursor)
 	mouse_exited.connect(_reset_cursor)
 	
-	# tem que resolver
 	#col_area.connect("input_event", _object_is_held)
+	pressed.connect(_on_pressed)
+	button_up.connect(_on_button_up)
 	
 	if item_type == "notebook":
 		TimelineManager.n_interaction_ref = self
@@ -36,18 +37,39 @@ func _reset_cursor() -> void:
 	Input.set_custom_mouse_cursor(CursorManager.default_icon)
 	
 ## Função para mudar o ícone para segurando enquanto o jogador estiver segurando enquanto está com o mouse no item
-func _object_is_held(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event.is_action_pressed("interact_with_items"):
-		if scene_ref:
-			scene_ref._on_item_interacted(self)
+#func _object_is_held(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	#if event.is_action_pressed("interact_with_items"):
+		#if scene_ref:
+			#scene_ref._on_item_interacted(self)
+		#Input.set_custom_mouse_cursor(CursorManager.grab_icon)
+		#object_held = true
+	#if event.is_action_released("interact_with_items") and object_held:
+		#pass
+		#
+		#object_held = false
+		#if open_after_interaction and PuzzleManager.puzzle_started:
+			#for i in scene_ref.interactable_items.get_children():
+				#if i != notebook:
+					#i.visible = false
+		#if delete_after_interaction:
+			#queue_free() # Destrói o item
+		#_reset_cursor() 
+
+func _on_pressed() -> void:
+	if scene_ref:
+		scene_ref._on_item_interacted(self)
 		Input.set_custom_mouse_cursor(CursorManager.grab_icon)
 		object_held = true
-	if event.is_action_released("interact_with_items") and object_held:
-		object_held = false
-		if open_after_interaction and PuzzleManager.puzzle_started:
-			for i in scene_ref.interactable_items.get_children():
-				if i != notebook:
-					i.visible = false
-		if delete_after_interaction:
-			queue_free() # Destrói o item
-		_reset_cursor() 
+	
+func _on_button_up() -> void:
+	if !object_held:
+		return
+	
+	object_held = false
+	if open_after_interaction and PuzzleManager.puzzle_started:
+		for i in scene_ref.interactable_items.get_children():
+			if i != notebook:
+				i.visible = false
+	if delete_after_interaction:
+		queue_free() # Destrói o item
+	_reset_cursor()
